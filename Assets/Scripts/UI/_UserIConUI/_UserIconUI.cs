c2f1b5c6c84b783a3152e8071d8f350122267d73
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-06-15 18:21:53 +0900

    Make : EnemySO

---
+++ Assets/Scripts/UI/_UserIConUI/_UserIconUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class _UserIconUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI powerText;
+
+    private void Start()
+    {
+        
+    }
+
+}
