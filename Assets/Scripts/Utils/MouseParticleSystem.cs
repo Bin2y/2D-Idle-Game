91b2b266c275f67992815205a814149aec3c9e9f
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-06-17 14:40:59 +0900

    Add : Simple Icon Pack Assets

--- Assets/Scripts/Utils/MouseParticleSystem.cs
+++ Assets/Scripts/Utils/MouseParticleSystem.cs
@@ -10,12 +10,13 @@ public class MouseParticleSystem : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Vector3 mousePos = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
             GameObject go = (GameObject)Instantiate(particle);
             float x = Random.Range(-1f, 1f);
             float y = Random.Range(-1f, 1f);
             float z = Random.Range(-1f, 1f);
 
-            go.transform.position = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+            go.transform.position = new Vector2(mousePos.x + x, mousePos.y + y);
             if (go.TryGetComponent<ParticleSystem>(out ParticleSystem ps))
             {
                 float duration = ps.main.duration;
