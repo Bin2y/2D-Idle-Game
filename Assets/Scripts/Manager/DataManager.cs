60a16eed3a82d584155bb528e9958cf8f7a3c93e
Bin2y <37493119+Bin2y@users.noreply.github.com>
2024-06-18 11:10:26 +0900

    Feat : PlayerData save

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 //전역에서 접근하여 데이터를 저장할 수 있도록
@@ -7,15 +10,47 @@ public class DataManager : Singleton<DataManager>
 {
     public PlayerSO playerData;
     public EnemySO enemyData;
+    public int waveData;
+    public BigInteger goldData;
+
+    private readonly string PlayerDataPath = "PlayerData.json";
 
     public void OnSaveButtonClicked()
     {
-        
+        Save();
+    }
+
+    public void OnLoadButtonClicked()
+    {
+        Load();
     }
 
     public void Save()
     {
         playerData = GameManager.Instance.player.data;
         enemyData = GameManager.Instance.enemy.data;
+        waveData = GameManager.Instance.wave;
+        goldData = GameManager.Instance.gold;
+        SavePlayerData();
+    }
+
+    public void Load()
+    {
+        LoadPlayerData();
+    }
+
+    public void SavePlayerData()
+    {
+        string data = JsonUtility.ToJson(playerData);
+        File.WriteAllText(Path.Combine(Application.dataPath, PlayerDataPath), data);
+        Debug.Log("Save Player Data");
+    }
+
+    public void LoadPlayerData()
+    {
+        string path = Path.Combine(Application.dataPath, PlayerDataPath);
+        string data = File.ReadAllText(path);
+        playerData = JsonUtility.FromJson<PlayerSO>(data);
+        Debug.Log("Load Player Data");
     }
 }
